b9d159de2858406322143ad8f24d03ffbe073476
VaishaliPari <vp1096@nyu.edu>
2020-04-07 12:35:01 -0400

    Add files via upload

---
+++ src/main/java/com/example/ConfigServer/ConfigServerApplication.java
@@ -0,0 +1,20 @@
+package com.example.ConfigServer;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Configuration;
+
+@SpringBootApplication
+@EnableConfigServer
+@EnableEurekaClient
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+}
